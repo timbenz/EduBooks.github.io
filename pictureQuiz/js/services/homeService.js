d57ca18744e34b1bdbc7c3b36367f420a3bde786
timbenz <44660621+timbenz@users.noreply.github.com>
2019-02-09 09:56:35 -0800

    Delete homeService.js

--- pictureQuiz/js/services/homeService.js
+++
@@ -1,9 +0,0 @@
-angular.module('pictureQuiz')
-.service('homeService', function ($http) {
-  this.getQuizzes = function(quizzesUrl) {
-    return $http({
-      method: 'GET',
-      url: quizzesUrl
-    });
-  };
-});
